22a895f3b35c748c7396a2eb2e3ce83b77e7b103
João Mauricio <joaomauricio.dev@gmail.com>
2025-04-23 20:10:18 -0300

    chore: add gingonic lib

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	
+	s := gin.Default()
+	s.GET("/teste", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "teste gin",
+		})
+	})
+
+	s.Run(":8000")
 }
