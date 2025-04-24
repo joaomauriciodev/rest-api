d852c103511d22100f598af41203cd938f664835
João Mauricio <joaomauricio.dev@gmail.com>
2025-04-23 21:43:23 -0300

    feat: implements product usecase

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/joaomauriciodev/rest-api/controller"
+	"github.com/joaomauriciodev/rest-api/usecase"
 )
 
 func main() {
 	s := gin.Default()
 
-	productController := controller.NewProductController()
+	productUsecase := usecase.NewProductUsecase()
+	productController := controller.NewProductController(productUsecase)
 
 	s.GET("/api/products", productController.GetProducts)
 
